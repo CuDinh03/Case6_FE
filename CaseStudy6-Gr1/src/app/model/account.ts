6d0987a26c75f5fbb180f957e8877993541fbc6c
Sonhr1403 <sonhr1403@gmail.com>
2023-01-06 09:43:11 +0700

    up lai

--- CaseStudy6-Gr1/src/app/model/account.ts
+++ CaseStudy6-Gr1/src/app/model/account.ts
@@ -1,12 +1,27 @@
-export interface Account{
-id: number;
-username: string;
-password: string;
-email: string;
-firstname: string;
-lastname: string;
-phone: string;
-address: string;
-birthday: Date;
-gender: string;
+export class Account {
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  phone: string;
+  address: string;
+  birthday: Date;
+  gender: string;
+
+  constructor(id: number, username: string, password: string, email: string, firstname: string, lastname: string, phone: string, address: string, birthday: Date, gender: string)
+
+{
+  this.id = id;
+  this.username = username;
+  this.password = password;
+  this.email = email;
+  this.firstname = firstname;
+  this.lastname = lastname;
+  this.phone = phone;
+  this.address = address;
+  this.birthday = birthday;
+  this.gender = gender;
+}
 }
--- CaseStudy6-Gr1/src/app/model/status.ts
+++ CaseStudy6-Gr1/src/app/model/status.ts
@@ -1,9 +1,17 @@
 import { Account } from "./account";
 
-export interface Status{
+export class Status{
   id: number;
   content: string;
   postDay: string
   status: string;
   owner: Account;
+
+  constructor(id: number, content: string, postDay: string, status: string, owner: Account){
+    this.id = id;
+    this.content = content;
+    this.postDay = postDay;
+    this.status = status;
+    this.owner = owner;
+  }
 }
