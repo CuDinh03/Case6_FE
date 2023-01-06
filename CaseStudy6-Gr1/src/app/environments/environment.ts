d0b083f35109d8a68824debc81493d4f43cf8640
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2023-01-06 11:54:07 +0700

    duc update

---
+++ CaseStudy6-Gr1/src/app/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/',
+  firebaseConfig: {
+    apiKey: "AIzaSyDUaBbdIV90k3yk-fyUe467G-xajOeINmw",
+    authDomain: "socialnetwork-cb9cc.firebaseapp.com",
+    databaseURL: "https://socialnetwork-cb9cc-default-rtdb.firebaseio.com/",
+    projectId: "socialnetwork-cb9cc",
+    storageBucket: "socialnetwork-cb9cc.appspot.com",
+    messagingSenderId: "52946647881",
+    appId: "1:52946647881:web:fbe0737e06b7aeb3eeb898",
+    measurementId: "G-XKPR9XRX4T"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
---
+++ CaseStudy6-Gr1/src/app/model/user.ts
@@ -0,0 +1,18 @@
+import {Role} from "./role";
+import {Img} from "./img";
+
+export interface User {
+  id: number;
+  userName: string;
+  password: string;
+  email: string;
+  phoneNumber: string;
+  birthday: string;
+  fistName: string;
+  lastName: string;
+  address: string;
+  gender: string;
+  img: [Img];
+  role: [Role];
+
+}
