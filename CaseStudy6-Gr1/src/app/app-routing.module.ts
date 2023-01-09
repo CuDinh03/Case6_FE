bccfd177fff5c1364f2c06a78af766225e588432
TranPhong04 <phongy3ulinh@gmail.com>
2023-01-09 23:08:32 +0700

    fe

--- CaseStudy6-Gr1/src/app/app-routing.module.ts
+++ CaseStudy6-Gr1/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {Router, RouterModule, Routes} from '@angular/router';
+import { ProfileComponent } from './friends/profile/profile.component';
+
 import {PageMainComponent} from "./page/page-main/page-main.component";
 import {PageProfileComponent} from "./page/page-profile/page-profile.component";
 import {SigninSignupComponent} from "./page/signin-signup/signin-signup.component";
@@ -8,11 +10,15 @@ const routes: Routes = [
   {path: 'main',component: PageMainComponent},
   {path: 'profile', component: PageProfileComponent},
   {path: '', component: SigninSignupComponent},
-
+  {path: 'showProfile',component:ProfileComponent}
   ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor() {
+  }
+
+}
