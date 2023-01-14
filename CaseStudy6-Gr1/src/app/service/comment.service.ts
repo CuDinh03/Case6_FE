5c238feba8e3add3d7f6206730d74d6b174b6cc4
CuDinh03 <dinhhuycu0305@gmail.com>
2023-01-15 06:29:37 +0700

    cudinh up comment

--- CaseStudy6-Gr1/src/app/service/comment.service.ts
+++ CaseStudy6-Gr1/src/app/service/comment.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {comment} from "../model/comment";
+import {Status} from "../model/status";
+import {Comment} from "@angular/compiler";
+
+
+const API_URL = `http://localhost:8080`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(private  http: HttpClient) { }
+
+  saveComment(comment: any): Observable<any>{
+    return this.http.post<comment>(API_URL+ '/comments', comment)
+  }
+
+  findCommentByStatusId(id:number): Observable<any>{
+    return this.http.get<comment[]>(`${API_URL}/comments/${id}`)
+  }
+
 }
