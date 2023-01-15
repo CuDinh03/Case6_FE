e074333464e244df14fa6f74571d82a38caeab71
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2023-01-15 14:43:14 +0700

    update getalluser

--- CaseStudy6-Gr1/src/app/page/page-admin/page-admin.component.ts
+++ CaseStudy6-Gr1/src/app/page/page-admin/page-admin.component.ts
@@ -3,6 +3,7 @@ import {Router} from "@angular/router";
 import {StatusService} from "../../service/status.service";
 import {AdminService} from "../../account/Admin/admin.service";
 import {Account} from "../../model/account";
+import {Friend} from "../../model/friend";
 
 @Component({
   selector: 'app-page-admin',
@@ -11,15 +12,13 @@ import {Account} from "../../model/account";
 })
 export class PageAdminComponent implements OnInit {
 
-  listAccount: Account[] = []
-
-
+  listAccount: any;
   constructor(private router: Router, private statusService: StatusService, private adminService: AdminService) {
   }
 
   ngOnInit(): void {
     this.adminService.getAllUser().subscribe((data) => {
-      this.listAccount = [data];
+      this.listAccount = data;
     })
   }
 }
